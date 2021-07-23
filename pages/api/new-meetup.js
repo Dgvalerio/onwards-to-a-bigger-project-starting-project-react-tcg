98ee0048e63f336e6c3f394f117a01a2956e6d4f
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 10:35:05 -0300

    Class 339 - Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,26 @@
-/* eslint-disable no-unused-vars */
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.map === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://dgvalerio:eRY1RrtpOPm8xxfQ@cluster0.sshuh.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    const meetupsCollections = db.collections('meetups');
+
+    const result = await meetupsCollections.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({
+      message: 'Meetup inserted!',
+    });
   }
 };
 
