91f787cc601403a63723249434ddf17dd4637714
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 07:37:15 -0300

    Class 327 - Outputting a List of Meetups

--- components/meetups/MeetupItem.jsx
+++ components/meetups/MeetupItem.jsx
@@ -1,23 +1,21 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
-function MeetupItem(props) {
-  return (
-    <li className={classes.item}>
-      <Card>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
-        </div>
-        <div className={classes.actions}>
-          <button>Show Details</button>
-        </div>
-      </Card>
-    </li>
-  );
-}
+const MeetupItem = ({ image, title, address }) => (
+  <li className={classes.item}>
+    <Card>
+      <div className={image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+      </div>
+      <div className={classes.actions}>
+        <button type="button">Show Details</button>
+      </div>
+    </Card>
+  </li>
+);
 
 export default MeetupItem;
---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_ITEMS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12445 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
+const HomePage = () => <MeetupList meetups={DUMMY_ITEMS} />;
+
+export default HomePage;
