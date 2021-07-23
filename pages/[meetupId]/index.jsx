2483fb07839d73cea6427d826bbbb85c86a32105
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 12:15:16 -0300

    Class 341 - Getting Data From The Database

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,5 +1,23 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
-import { DUMMY_ITEMS } from '../index';
+
+const DUMMY_ITEMS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12445 Some City',
+    description: 'This is a second meetup!',
+  },
+];
 
 const MeetupDetails = ({ meetupData }) => {
   const { title, address, description, image } = meetupData;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,6 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
-export const DUMMY_ITEMS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12445 Some City',
-    description: 'This is a second meetup!',
-  },
-];
+import MeetupList from '../components/meetups/MeetupList';
 
 const HomePage = ({ meetups }) => <MeetupList meetups={meetups} />;
 
@@ -32,11 +15,30 @@ const HomePage = ({ meetups }) => <MeetupList meetups={meetups} />;
 //   };
 // };
 
-export const getStaticProps = async () => ({
-  props: {
-    meetups: DUMMY_ITEMS,
-  },
-  revalidate: 10, // Time in seconds
-});
+export const getStaticProps = async () => {
+  const client = await MongoClient.connect(
+    'mongodb+srv://dgvalerio:eRY1RrtpOPm8xxfQ@cluster0.sshuh.mongodb.net/meetups?retryWrites=true&w=majority'
+  );
+
+  const db = client.db();
+
+  const meetupsCollections = db.collection('meetups');
+
+  const meetups = await meetupsCollections.find().toArray();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        // eslint-disable-next-line no-underscore-dangle
+        id: meetup._id.toString(),
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        description: meetup.description,
+      })),
+    },
+    revalidate: 10, // Time in seconds
+  };
+};
 
 export default HomePage;
