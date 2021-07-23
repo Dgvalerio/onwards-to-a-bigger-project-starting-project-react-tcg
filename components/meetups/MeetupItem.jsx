3baaa3aa50853df1df9ef6b2514439fcec82acf7
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 08:07:15 -0300

    Class 330 - Using Programmatic (Imperative) Navigation

--- components/meetups/MeetupItem.jsx
+++ components/meetups/MeetupItem.jsx
@@ -1,21 +1,31 @@
+import { useRouter } from 'next/router';
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
-const MeetupItem = ({ image, title, address }) => (
-  <li className={classes.item}>
-    <Card>
-      <div className={image}>
-        <img src={image} alt={title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-      </div>
-      <div className={classes.actions}>
-        <button type="button">Show Details</button>
-      </div>
-    </Card>
-  </li>
-);
+const MeetupItem = ({ image, title, address, id }) => {
+  const router = useRouter();
+
+  const showDetailsHandler = () => router.push(`/${id}`);
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+        </div>
+        <div className={classes.actions}>
+          <button type="button" onClick={showDetailsHandler}>
+            Show Details
+          </button>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default MeetupItem;
